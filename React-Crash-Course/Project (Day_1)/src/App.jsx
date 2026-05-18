a80e6d3e027b2d42a468828b8e1b38fcb4b9b1de
Dheeraj Parihar <dheerajparihar02001@gmail.com>
2026-05-18 11:55:09 +0530

    create new components

---
+++ React-Crash-Course/Project (Day_1)/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+// import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <section className="center">
+
+      </section>
+    </>
+  )
+}
+
+export default App

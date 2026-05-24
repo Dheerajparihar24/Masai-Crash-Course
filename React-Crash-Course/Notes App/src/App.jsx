54b08f6f9a134da784c9192b2aedb416fda30505
Dheeraj Parihar <dheerajparihar02001@gmail.com>
2026-05-24 13:12:39 +0530

    localStorage added

--- React-Crash-Course/Notes App/src/App.jsx
+++ React-Crash-Course/Notes App/src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos"));
+    return storedTodos ? JSON.parse(localStorage.getItem("todos")) : [];
+  });
+
+  // 1 Load todos when app starts (Runs when app opens)
+
+  //2 Save todos whenever todos changes
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+  //   This runs whenever:
+  // ✅ add task
+  // ✅ edit task
+  // ✅ delete task
+  // ✅ toggle task
+  // ✅ clear task
 
   const handleDelete = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));

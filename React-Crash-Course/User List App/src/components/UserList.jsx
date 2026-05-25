86d546add5661a7cd2d2c5b224d7d9db0c455275
Dheeraj Parihar <dheerajparihar02001@gmail.com>
2026-05-25 09:40:07 +0530

    Updates in search

--- React-Crash-Course/User List App/src/components/UserList.jsx
+++ React-Crash-Course/User List App/src/components/UserList.jsx
@@ -1,9 +1,15 @@
 import UserCard from "./UserCard";
 
 export default function UserList({ users, searchQuery }) {
-  const filteredUsers = users.filter((user) =>
-    user.name.toLowerCase().includes(searchQuery.toLowerCase()),
+  const filteredUsers = users.filter(
+    (user) =>
+      user.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      user.email.toLowerCase().includes(searchQuery.toLowerCase()),
   );
+
+  if (filteredUsers.length === 0) {
+    return <h2>No users found.</h2>;
+  }
   return (
     <section className="user-list">
       {filteredUsers.map((user) => (

30bbd4972ffe26968295e685939ce511a151fc20
Dheeraj Parihar <dheerajparihar02001@gmail.com>
2026-05-28 12:05:05 +0530

    updates in signup and authContext

--- React-Crash-Course/taskflow/src/context/AuthContext.jsx
+++ React-Crash-Course/taskflow/src/context/AuthContext.jsx
@@ -2,15 +2,11 @@ import { createContext, useState } from "react";
 import { auth } from "../firebase/config";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
-// Step-1 :createContext
 export const AuthContext = createContext();
 
-//Step-2 : create context Provider -> Rap App.js with AuthProvider
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
 
-  // Register new user / Signup-------
   const signup = async (email, password) => {
     try {
       const userCredential = await createUserWithEmailAndPassword(
@@ -34,22 +30,24 @@ export const AuthProvider = ({ children }) => {
   };
 
   const login = (userData) => {
-    console.log("login user");
-    // console.log(userData)
     setUser(userData);
   };
 
   const logout = () => {
-    console.log("Logout");
     setUser(null);
   };
 
-  const resetPassword = () => {
-    console.log("reset-user");
-  };
+  const resetPassword = () => {};
+
   return (
     <AuthContext.Provider
-      value={{ user, loading, setUser, login, signup, logout, resetPassword }}
+      value={{
+        user,
+        signup,
+        login,
+        logout,
+        resetPassword,
+      }}
     >
       {children}
     </AuthContext.Provider>
--- React-Crash-Course/taskflow/src/pages/Signup.jsx
+++ React-Crash-Course/taskflow/src/pages/Signup.jsx
@@ -4,31 +4,32 @@ import { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 export default function Signup() {
+  const { signup } = useContext(AuthContext);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [conformPassword, setConformPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
-
-  const { signup } = useContext(AuthContext);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    setLoading("");
     setError("");
 
-    if (password !== conformPassword) {
-      setError("Password do not match");
-      setLoading(false);
-      return;
+    if (password !== confirmPassword) {
+      return setError("Passwords do not match");
     }
+
+    setLoading(true);
 
     const result = await signup(email, password);
 
     if (!result.success) {
       setError(result.message);
     }
+
     setLoading(false);
   };
 
@@ -53,31 +54,33 @@ export default function Signup() {
           <input
             type="password"
             id="password"
-            placeholder="Enter your password"
+            placeholder="Enter password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
+
         <div className="form-group">
-          <label htmlFor="conform-password">Conform Password</label>
+          <label htmlFor="confirm-password">Confirm Password</label>
           <input
             type="password"
-            id="conform-password"
-            placeholder="Conform password"
-            value={conformPassword}
-            onChange={(e) => setConformPassword(e.target.value)}
+            id="confirm-password"
+            placeholder="Confirm password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </div>
+
+        {error && <p style={{ color: "red" }}>{error}</p>}
 
         <button type="submit" className="btn" disabled={loading}>
           {loading ? "Creating..." : "Signup"}
         </button>
 
         <div className="form-links">
-          <Link to="/login">Already have account? Login</Link>
+          <Link to="/login">Already have an account? Login</Link>
         </div>
       </form>
-      {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
 }
